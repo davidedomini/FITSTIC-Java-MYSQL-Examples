377a9b03cbdceb2734e7179bffcc6bc938b26e08
dom <davide.domini@unibo.it>
2024-02-18 17:28:01 +0100

    deps: add mysql connector dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,15 @@ repositories {
 }
 
 dependencies {
+    implementation("mysql:mysql-connector-java:8.0.33")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.withType<JavaExec>().forEach {
+    it.args("-Djava.net.preferIPv4Stack=true")
 }
