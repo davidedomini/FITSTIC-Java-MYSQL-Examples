0e3fdee688b0bcb08ade1ec9b3872605e73026c3
dom <davide.domini@unibo.it>
2024-02-18 17:27:40 +0100

    feat: add basic connection

---
+++ src/main/java/fitstic/BasicConnection/Main.java
@@ -0,0 +1,28 @@
+package fitstic.BasicConnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Connection connection = null;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver")
+                .getDeclaredConstructor()
+                .newInstance();
+            connection = DriverManager.getConnection(
+                "jdbc:mysql://172.17.0.2:3306/university",
+                "root",
+                "root"
+            );
+
+            System.out.println("Connected!");
+        } catch (Exception e){
+            System.out.println("SQLException: " + e.getMessage());
+        }
+
+    }
+}
