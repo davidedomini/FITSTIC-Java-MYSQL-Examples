c24558e5581f10bf2a58294b89beb1034f03c2de
dom <davide.domini@unibo.it>
2024-02-18 18:26:12 +0100

    feat: add simple select example

---
+++ src/main/java/fitstic/SimpleQuery/QueryWithError.java
@@ -0,0 +1,38 @@
+package fitstic.SimpleQuery;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class QueryWithError {
+
+    public static void main(String[] args){
+        Connection connection = null;
+        String query = "SELECT Name FROM Students";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver")
+                    .getDeclaredConstructor()
+                    .newInstance();
+
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://172.17.0.2:3306/university",
+                    "root",
+                    "root"
+            );
+
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery(query);
+            while(resultSet.next()){
+                System.out.println(resultSet.getString("Surname"));
+            }
+
+            connection.close();
+        } catch (Exception e){
+            System.out.println("SQLException: " + e.getMessage());
+        }
+
+    }
+}
---
+++ src/main/java/fitstic/SimpleQuery/SimpleQuery.java
@@ -0,0 +1,39 @@
+package fitstic.SimpleQuery;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class SimpleQuery {
+
+    public static void main(String[] args){
+        Connection connection = null;
+        String query = "SELECT Name FROM Students";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver")
+                    .getDeclaredConstructor()
+                    .newInstance();
+
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://172.17.0.2:3306/university",
+                    "root",
+                    "root"
+            );
+
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery(query);
+            while(resultSet.next()){
+                System.out.println(resultSet.getString("Name"));
+            }
+
+            connection.close();
+        } catch (Exception e){
+            System.out.println("SQLException: " + e.getMessage());
+        }
+
+    }
+
+}
